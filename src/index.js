e5427889e6a3acfdf1f54480cc70fb44db4d1857
tanzimzaki <tanzimzaki@gmail.com>
2021-11-03 17:55:44 +0800

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
+const toDoForm = document.getElementById('create-task-form');
+const toDoFormInput = document.getElementById('new-task-description');
+const toDoItemContainer = document.getElementById('tasks');  
+
+let captureToDoInputValue = () => {
+  return toDoFormInput.value
+}
+
+let formSubmit = (event) => {
+  event.preventDefault();
+  let input = toDoFormInput.value;
+  renderListItems(input);
+
+}
+
+let renderListItems = (toDoInput) => {
+  let li = document.createElement('li');
+  li.innerText = toDoInput
+  toDoItemContainer.appendChild(li) 
+}
+
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  toDoForm.addEventListener('submit', formSubmit)
+
 });
